f87e31b06ba4aba0432d87d140838a30541e2d30
canberkelmal <elmalcanberk@gmail.com>
2022-04-29 00:29:42 +0300

    Counter_ColorBoundedForce_Coordinate

--- Assets/Scripts/PlaayerMovement.cs
+++ Assets/Scripts/PlaayerMovement.cs
@@ -4,9 +4,7 @@ public class PlaayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
-
-    public float forwardForce = 500f;
-    public float sidewaysForce = 500f;
+    public float force=500f;
 
     // Start is called before the first frame update
     void Start()
@@ -22,19 +20,19 @@ public class PlaayerMovement : MonoBehaviour
         //rb.AddForce(0,0,forwardForce*Time.deltaTime);
 
         if (Input.GetKey("d")){        
-            rb.AddForce(sidewaysForce*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            rb.AddForce(force*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         };
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-sidewaysForce*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            rb.AddForce(-force*Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         };
         if (Input.GetKey("w"))
         {
-            rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+            rb.AddForce(0, 0, force * Time.deltaTime, ForceMode.VelocityChange);
         };
         if (Input.GetKey("s"))
         {
-            rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+            rb.AddForce(0, 0, -force * Time.deltaTime, ForceMode.VelocityChange);
         };
 
     }
--- Assets/Scripts/PlayerCollision.cs
+++ Assets/Scripts/PlayerCollision.cs
@@ -2,13 +2,50 @@ using UnityEngine;
 
 public class PlayerCollision : MonoBehaviour
 {
+    public GameObject count;
+    public PlaayerMovement force;
+    int cc=0;
+    public GameObject theBox;
+    public GameObject box=GameObject.Find("box");
+    public GameObject box1=GameObject.Find("box (1)");
+    public GameObject box2= GameObject.Find("box (2)");
+    public GameObject plane= GameObject.Find("Plane");
+    public GameObject plane2= GameObject.Find("Plane (2)");
+    public GameObject speed=GameObject.Find("speed");
+
     // Start is called before the first frame update
     void OnCollisionEnter(Collision ColInf){
+        count=GameObject.Find("count");
         
         Debug.Log("Collision! " + ColInf.collider.name);
         
-        if(ColInf.collider.name!="Plane")
-        GetComponent<Renderer>().material.color = ColInf.collider.GetComponent<Renderer>().material.GetColor("_Color");
+        if(ColInf.collider.name!="Plane"){
+        theBox=GameObject.Find("ColInf.collider.name");
+            cc++;
+            GetComponent<Renderer>().material.color = ColInf.collider.GetComponent<Renderer>().material.GetColor("_Color");
+            count.GetComponent<UnityEngine.UI.Text>().text = "" + cc;
 
+            /* for(int i=count.GetComponent<UnityEngine.UI.Text>().fontSize; i<count.GetComponent<UnityEngine.UI.Text>().fontSize+10; i++){
+                count.GetComponent<UnityEngine.UI.Text>().fontSize=i;
+            }
+            for(int i=count.GetComponent<UnityEngine.UI.Text>().fontSize+10; i>=count.GetComponent<UnityEngine.UI.Text>().fontSize; i++){
+                count.GetComponent<UnityEngine.UI.Text>().fontSize=i;
+            } animation*/
+        }
+        if(GetComponent<Renderer>().material.color==box.GetComponent<Renderer>().material.GetColor("_Color")){
+        force.force=25;
+        speed.GetComponent<UnityEngine.UI.Text>().text = force.force.ToString();}
+        if(GetComponent<Renderer>().material.color==box1.GetComponent<Renderer>().material.GetColor("_Color")){
+        force.force=10;
+        speed.GetComponent<UnityEngine.UI.Text>().text = force.force.ToString();}
+        if(GetComponent<Renderer>().material.color==box2.GetComponent<Renderer>().material.GetColor("_Color")){
+        force.force=15;
+        speed.GetComponent<UnityEngine.UI.Text>().text = force.force.ToString();}
+        if(GetComponent<Renderer>().material.color==plane.GetComponent<Renderer>().material.GetColor("_Color")){
+        force.force=20;
+        speed.GetComponent<UnityEngine.UI.Text>().text = force.force.ToString();}
+        if(GetComponent<Renderer>().material.color==plane2.GetComponent<Renderer>().material.GetColor("_Color")){
+        force.force=50;
+        speed.GetComponent<UnityEngine.UI.Text>().text = force.force.ToString();}
     }
 }
